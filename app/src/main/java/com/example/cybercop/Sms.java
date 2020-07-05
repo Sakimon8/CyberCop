78bb98ea9f3b60d24889b4f32cb2bd67cfc801c9
Deepikashini16 <deepikashini16@gmail.com>
2020-07-05 23:27:53 +0530

    i have updated in gpstracker.java

--- app/src/main/java/com/example/cybercop/Sms.java
+++ app/src/main/java/com/example/cybercop/Sms.java
@@ -25,7 +25,7 @@ public class Sms extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sms);
         myDB = new DatabaseHelper(this);
-
+//Comment
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.RECEIVE_SMS)!= PackageManager.PERMISSION_GRANTED)
         {
             requestPermissions(new String[]{Manifest.permission.RECEIVE_SMS},1000);
