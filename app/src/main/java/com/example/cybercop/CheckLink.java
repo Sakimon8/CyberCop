5038b6059faa486c695f0fcc45d905083aa19da8
Deepikashini16 <deepikashini16@gmail.com>
2020-07-08 23:22:16 +0530

    checklink2 added

--- app/src/main/java/com/example/cybercop/CheckLink.java
+++ app/src/main/java/com/example/cybercop/CheckLink.java
@@ -17,6 +17,7 @@ import java.net.URL;
 public class CheckLink extends AsyncTask {
     private static final int REQUEST_CODE = 000;
     // public DatabaseHelper myDB;
+    public String string;
     public String msg,msg_from;
     private Context context;
     private int byGetOrPost = 0;
@@ -28,25 +29,20 @@ public class CheckLink extends AsyncTask {
     protected Object doInBackground(Object[] args) {
         try{
              msg = String.valueOf(args[0]);
-           //  msg_from=String.valueOf(args[1]);
-           // Toast.makeText(context,""+msg_from,Toast.LENGTH_LONG).show();
-
-          //  String replacemsg=msg.replace(" ","%20");
-            URL aURL = new URL(msg);
-
-//                    System.out.println("protocol = " + aURL.getProtocol());
-//                    System.out.println("authority = " + aURL.getAuthority());
-//                    System.out.println("host = " + aURL.getHost());
-//                    System.out.println("port = " + aURL.getPort());
-//                    System.out.println("path = " + aURL.getPath());
-//                    System.out.println("query = " + aURL.getQuery());
-//                    System.out.println("filename = " + aURL.getFile());
-//                    System.out.println("ref = " + aURL.getRef());
-         //   Toast.makeText(getApplicationContext(),"Inside Try"+aURL,Toast.LENGTH_LONG).show();
-            String string= msg.replace("https://","").replace("https:// www.","").replace("www.","");
-           // Toast.makeText(context,""+string,Toast.LENGTH_LONG).show();
+//            URL aURL = new URL(msg);
+//
+//            System.out.println("protocol = " + aURL.getProtocol());
+//            System.out.println("authority = " + aURL.getAuthority());
+//            System.out.println("host = " + aURL.getHost());
+//            System.out.println("port = " + aURL.getPort());
+//            System.out.println("path = " + aURL.getPath());
+//            System.out.println("query = " + aURL.getQuery());
+//            System.out.println("filename = " + aURL.getFile());
+//            System.out.println("ref = " + aURL.getRef());
+            string= msg.replace("https://","").replace("https:// www.","").replace("www.","").replace("http://","").replace("http:// www.","");
 
             String link = "https://link-detection.herokuapp.com/?bla="+string;
+         //  String link="https://link-detection.herokuapp.com/?bla=purplehorses.net/?page=bleach-244-online";
             URL url = new URL(link);
             HttpClient client = new DefaultHttpClient();
             HttpGet request = new HttpGet();
@@ -78,24 +74,13 @@ public class CheckLink extends AsyncTask {
     }
     @Override
     protected void onPostExecute(Object result){
-        Toast.makeText(context,"The Level of maicous is "+String.valueOf(result),Toast.LENGTH_LONG).show();
-        if(String.valueOf(result).equals("Spam"))
-        {
-           // addtodb(msg,msg_from);
-        }
+        Toast.makeText(context,"The Level of maliciousness of  "+string+"  is  "+String.valueOf(result),Toast.LENGTH_LONG).show();
+
 
 
     }
 
-    private void addtodb(String msg,String msg_from) {
-        DatabaseHelper myDB=new DatabaseHelper(context);
-        boolean insertData = myDB.addData(msg,msg_from);
-        if(insertData==true){
-            Toast.makeText(context, "Data  from "+msg_from+"Successfully Inserted!", Toast.LENGTH_LONG).show();
-            // viewItems();
-        }else{
-            Toast.makeText(context, "Something went wrong :(.", Toast.LENGTH_LONG).show();
-        }
-    }
+
+
 
 }
--- app/src/main/java/com/example/cybercop/Safe_Search.java
+++ app/src/main/java/com/example/cybercop/Safe_Search.java
@@ -15,6 +15,19 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 public class Safe_Search extends AppCompatActivity  {
     WebView myWebView ;
     @Override
@@ -27,13 +40,14 @@ public class Safe_Search extends AppCompatActivity  {
             @Override
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
                 super.onPageStarted(view, url, favicon);
+              //  Toast.makeText(getApplicationContext(),"Before checklink"+url,Toast.LENGTH_SHORT).show();
+
                 new CheckLink(getApplicationContext(),0).execute(url);
 
-                Toast.makeText(getApplicationContext(),url,Toast.LENGTH_SHORT).show();
+              //  Toast.makeText(getApplicationContext(),url,Toast.LENGTH_SHORT).show();
 
                 Log.d("WebView", "your current url when webpage loading.." + url);
             }
-
             @Override
             public void onPageFinished(WebView view, String url) {
                // Toast.makeText(getApplicationContext(),url,Toast.LENGTH_SHORT).show();
@@ -49,13 +63,8 @@ public class Safe_Search extends AppCompatActivity  {
                 // TODO Auto-generated method stub
                 super.onLoadResource(view, url);
             }
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-               // Toast.makeText(getApplicationContext(),url,Toast.LENGTH_SHORT).show();
 
-                System.out.println("when you click on any interlink on webview that time you got url :-" + url);
-                return super.shouldOverrideUrlLoading(view, url);
-            }
+
         });
         WebSettings websettings=myWebView.getSettings();
         websettings.getJavaScriptEnabled();
